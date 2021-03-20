19c585e82ae1b743f5caaaa697899b8e75d4b172
Bayu Setiawan <bayu@skyshi.com>
2021-03-20 13:38:11 +0700

    minor modify quote

--- src/commons/quotes.js
+++ src/commons/quotes.js
@@ -1,7 +1,8 @@
 const ALLQUOTES = [];
 const makeQuote = (quote, author) => ALLQUOTES.push({quote, author});
 
-makeQuote("Those Who Doubt Your Ability Probably Have A Valid Reason","Bu Poor food and beverages");
+makeQuote("Besok tutup ya, mau libur dulu.","Bu Poor food and beverages");
+makeQuote("Those Who Doubt Your Ability Probably Have A Valid Reason","Internet");
 makeQuote("Tampilan jelek karena yang buat backend","Alesan Mainstream Enjiner");
 makeQuote("Try Hard And Don't Worry If You Fail Because Everyone Expected that","Mahatma Gandhi");
 makeQuote("Love Is Trash, Bitches Need Cash.","Mahatma Gandhi");
